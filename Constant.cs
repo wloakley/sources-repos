4624f69e770b1806d665e01e54fdd8357f96289e
wloakley <46609159+wloakley@users.noreply.github.com>
2020-11-10 20:40:09 -0600

    Add files via upload

---
+++ Constant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MuseumDataEntry
+{
+    public class Constant
+    {
+        public string url = "http://ccsdev02.ccs.ua.edu:8080/API-DEV/";
+    }
+}
