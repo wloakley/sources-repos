60100a3ef32d00ec423fba189759bab9f9e4e5b2
wloakley <46609159+wloakley@users.noreply.github.com>
2020-11-20 11:27:48 -0600

    Add files via upload

---
+++ IdentityController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MuseumDataEntry.Controllers
+{
+    public class IdentityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
