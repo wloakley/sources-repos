8368383e32f52e1def975fdb5f21fca08dd22e01
wloakley <46609159+wloakley@users.noreply.github.com>
2020-11-20 11:30:49 -0600

    Add files via upload

---
+++ IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MuseumDataEntry.Data;
+
+[assembly: HostingStartup(typeof(MuseumDataEntry.Areas.Identity.IdentityHostingStartup))]
+namespace MuseumDataEntry.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MuseumDataEntryContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DefaultConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MuseumDataEntryContext>();
+            });
+        }
+    }
+}
